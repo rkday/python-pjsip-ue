55682a242eb369dc966ecdba14bc63dab55ec528
Rob Day <rkd@rkd.me.uk>
2015-12-28 21:25:17 +0000

    Python3 support

--- sipue/ue.py
+++ sipue/ue.py
@@ -1,27 +1,32 @@
-from _example import ffi, lib
+from ._example import ffi, lib
 import logging
+
+__pdoc__ = {'ffi': None, 'lib': None}
 
 _log = logging.getLogger(__name__) 
 
-log_levels = {1: logging.ERROR,
-        2: logging.WARNING,
-        3: logging.INFO,
-        4: logging.DEBUG}
-
 @ffi.callback("void(int, const char*, int)")
 def logger_callback(level, data, length):
-    _log.log(log_levels.get(level, logging.DEBUG), ffi.string(data))
+
+    log_levels = {1: logging.ERROR,
+                  2: logging.WARNING,
+                  3: logging.INFO,
+                  4: logging.DEBUG}
+
+    _log.log(log_levels.get(level, logging.DEBUG), ffi.string(data).decode('ascii'))
 
 class UE(object):
-    def __init__(self, realm, uri, username, password, outbound_proxy=""):
-        self.ueptr = lib.ue_new(logger_callback, realm, uri, username, password, outbound_proxy)
-        if self.ueptr == ffi.NULL:
+    '''SIP user agent class, allowing emulation of a SIP softphone for testing.'''
+
+    def __init__(self, realm, uri, username, password, outbound_proxy=b""):
+        self._ueptr = lib.ue_new(logger_callback, realm, uri, username, password, outbound_proxy)
+        if self._ueptr == ffi.NULL:
             raise Exception("Could not create UE")
 
     def register(self, expiry=300):
-        rc = lib.ue_register(self.ueptr, expiry)
-        print "Got {} response to REGISTER".format(rc)
+        rc = lib.ue_register(self._ueptr, expiry)
+        _log.info("Got {} response to REGISTER".format(rc))
 
     def text(self, target, message):
-        rc = lib.ue_send_message(self.ueptr, target, message)
-        print "Got {} response to MESSAGE".format(rc)
+        rc = lib.ue_send_message(self._ueptr, target, message)
+        _log.info("Got {} response to MESSAGE".format(rc))
