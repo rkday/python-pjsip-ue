55aa2d7c5dfd0e0e865d4859a648526bd38ed912
Rob Day <rkd@rkd.me.uk>
2015-12-28 22:20:16 +0000

    Type annotations and return values

--- sipue/ue.py
+++ sipue/ue.py
@@ -18,15 +18,17 @@ def logger_callback(level, data, length):
 class UE(object):
     '''SIP user agent class, allowing emulation of a SIP softphone for testing.'''
 
-    def __init__(self, realm, uri, username, password, outbound_proxy=b""):
-        self._ueptr = lib.ue_new(logger_callback, realm, uri, username, password, outbound_proxy)
+    def __init__(self, realm: str, uri: str, username: str, password: str, outbound_proxy: str = ""):
+        self._ueptr = lib.ue_new(logger_callback, realm.encode('utf-8'), uri.encode('utf-8'), username.encode('utf-8'), password.encode('utf-8'), outbound_proxy.encode('utf-8'))
         if self._ueptr == ffi.NULL:
             raise Exception("Could not create UE")
 
-    def register(self, expiry=300):
+    def register(self, expiry: int = 300):
         rc = lib.ue_register(self._ueptr, expiry)
         _log.info("Got {} response to REGISTER".format(rc))
+        return rc
 
-    def text(self, target, message):
-        rc = lib.ue_send_message(self._ueptr, target, message)
+    def text(self, target: str, message: str):
+        rc = lib.ue_send_message(self._ueptr, target.encode('utf-8'), message.encode('utf-8'))
         _log.info("Got {} response to MESSAGE".format(rc))
+        return rc
